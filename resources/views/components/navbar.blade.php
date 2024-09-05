fd1cfc73297c991d87c0fe2d75e0a8ea12355311
Heagent Kenneth <160743178+kennetmakannasi@users.noreply.github.com>
2024-09-05 20:07:25 +0700

    Add files via upload

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,12 +4,12 @@
         <img src="{{ asset('img/Screenshot_2024-08-01_131330-removebg-preview.png') }}" class="w-36" alt="" srcset="">
     </div>
 
-    <div class="mt-8 text-xl text-white font-semibold ml-[700px]">
-        <a class="ml-7 hover:bg-indigo-900 hover:p-1 hover:rounded-lg" href="/home">Home</a>
-        <a class="ml-7 hover:bg-indigo-900 hover:p-1 hover:rounded-lg" href="/book">Book</a>
-        <a class="ml-7 hover:bg-indigo-900 hover:p-1 hover:rounded-lg" href="/category">Category</a>
+    <div class="mt-8 text-xl text-white font-semibold ml-[650px]">
+        <a class="ml-7 hover:underline hover: underline-offset-8" href="/home">Home</a>
+        <a class="ml-7 hover:underline hover: underline-offset-8" href="/book">Book</a>
+        <a class="ml-7 hover:underline hover: underline-offset-8" href="/category">Category</a>
     </div>
-    <div class="inline-flex mt-7 ml-10">
+    <div class="inline-flex mt-7 ml-5">
         <form action="{{ route('pwd.search') }}" method="get">
             <input class="w-52 h-7 bg-gradient-to-r from-slate-500 to-slate-400 mt-2 rounded-full text-white" type="text" name="search" id="search">
             <button class="ml-1 hover:bg-slate-400 absolute mt-2 hover:rounded-2xl" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"><path fill="white" d="m19.485 20.154l-6.262-6.262q-.75.639-1.725.989t-1.96.35q-2.402 0-4.066-1.663T3.808 9.503T5.47 5.436t4.064-1.667t4.068 1.664T15.268 9.5q0 1.042-.369 2.017t-.97 1.668l6.262 6.261zM9.539 14.23q1.99 0 3.36-1.37t1.37-3.361t-1.37-3.36t-3.36-1.37t-3.361 1.37t-1.37 3.36t1.37 3.36t3.36 1.37"/></svg></button>
